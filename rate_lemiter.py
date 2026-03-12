9edd2accc5ecb1ce97076b8f7bfdc19749374ae2
Amarshesh Sharma <amarsheshsit@rediffmail.com>
2026-03-12 14:11:24 +0530

    Added the rule class, made time stamp based and then seperated the classess logic

--- rate_lemiter.py
+++ rate_lemiter.py
@@ -1,24 +1,33 @@
+from time import time
+from rate_limiter_rules import RateLimiterRules
 class RateLimiter:
+    def __init__(self):
+        self.counter = {}
+        self.rules = RateLimiterRules(max_requests=5, time_window=300)
 
-    def __init__(self, user):
-        self.user = user
-        self.count = {}
-
-    def is_allowed(self):
-        if self.count.get(self.user, 0) >= 3:
-            return False
-        else:
-            self.count[self.user] = self.count.get(self.user, 0) + 1
+    def is_allowed(self, user):
+        if user not in self.counter:
+            print(f"User {user} is making the first request, so allowing it.")
+            timenow = time()
+            self.counter[user] = {"count": 1, "time": timenow}
             return True
+        else:
+            count = self.counter[user]["count"]
+            elapsed = time() - self.counter[user]["time"]
+            if elapsed > self.rules.time_window:
+                self.counter[user]["count"] = 1
+                self.counter[user]["time"] = time()
+                print(f"User came after {self.rules.time_window} seconds, so count is reset to 1")
+                return True
+            elif count < self.rules.max_requests and elapsed <= self.rules.time_window:
+                self.counter[user]["count"] += 1
+                print(f"Request is allowed. User has made {self.counter[user]['count']} requests in the last {int(elapsed)} seconds.")
+                return True
+            else:
+                print(f"Rate limit exceeded. Please try again later after {self.rules.time_window - int(elapsed)} seconds")
+                return False
 
-    def print_count(self):
-        print(f"User: {self.user}, Count: {self.count[self.user]}")
+    def print_count(self, user):
+        c = self.counter.get(user, {}).get("count", 0)
+        print(f"User: {user}, Count: {c}")
 
-
-# Example usage
-user1 = "User1"
-limiter1 = RateLimiter(user1)
-print(limiter1.is_allowed())  # 
-print(limiter1.is_allowed())  # 
-print(limiter1.is_allowed())  # 
-print(limiter1.is_allowed())  # True
